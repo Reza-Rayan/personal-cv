1abecacde17f1442d0000737eef5933d61b9e078
reza-rayan <rezarayan78@gmail.com>
2024-10-04 15:51:12 +0330

    Fix: skills error

--- src/app/pages/Home/components/Skills.tsx
+++ src/app/pages/Home/components/Skills.tsx
@@ -1,3 +1,4 @@
+import React, { CSSProperties } from "react";
 import { skills } from "../../../../@data/Skills.json";
 import { Skill } from "../../../../@types/Skill";
 // Custom Components
@@ -9,23 +10,27 @@ const Skills = () => {
     <section>
       <Title center>My Skills</Title>
       <ul className="grid grid-cols-2 lg:grid-cols-5 mt-10 gap-10">
-        {skills.map((skill: Skill) => (
-          <li key={skill.id}>
-            {" "}
-            <div
-              className="radial-progress odd:text-primary even:text-secondary flex flex-col items-center justify-center gap-4"
-              style={{
-                "--value": `${skill.percent}`,
-                "--size": "12rem",
-                "--thickness": "30px",
-              }}
-              role="progressbar"
-            >
-              <h3 className="font-bold text-lg">{skill.title}</h3>
-              <span className="font-bold text-xl"> {skill.percent}%</span>
-            </div>
-          </li>
-        ))}
+        {skills.map((skill: Skill) => {
+          // Define the style object
+          const style: CSSProperties = {
+            "--value": skill.percent,
+            "--size": "12rem",
+            "--thickness": "30px",
+          } as React.CSSProperties; // Use type assertion to allow custom properties
+
+          return (
+            <li key={skill.id}>
+              <div
+                className="radial-progress odd:text-primary even:text-secondary flex flex-col items-center justify-center gap-4"
+                style={style}
+                role="progressbar"
+              >
+                <h3 className="font-bold text-lg">{skill.title}</h3>
+                <span className="font-bold text-xl">{skill.percent}%</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
